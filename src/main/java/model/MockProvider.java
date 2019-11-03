92ca126a28568d9e50c4894e8788397c3ec204e6
Michał <m.tegi@outlook.com>
2019-11-03 14:59:38 +0100

    added login form

--- src/main/java/model/MockProvider.java
+++ src/main/java/model/MockProvider.java
@@ -8,7 +8,11 @@ public class MockProvider implements IDataProvider {
     @Override
     public void fill(DataContext data) {
         Author sapek = new Author("Andrzej","Sapkowski");
+        Author dzordz = new Author("Goerge R. R.","Martin");
         data.books.add(new Book("Miecz Przeznaczenia", sapek));
         data.books.add(new Book("Pani Jeziora", sapek));
+        data.books.add(new Book("Starcie królów",dzordz));
+        data.books.add(new Book("Nawałnica Mieczy",dzordz));
+        data.books.add(new Book("Gra o tron",dzordz));
     }
 }
